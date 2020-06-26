13b4aacc85e626fca2fb0897f13ce4e9a66748df
Jesús Castro <x51v4n@gmail.com>
2020-06-26 01:02:49 -0500

    Fix test case with cat
    This test case shows the entire output from the Python execution.
    Includes a test File.
    
    Signed-off-by: Jesús Castro <x51v4n@gmail.com>

--- matefacil/matefacil_demo/views.py
+++ matefacil/matefacil_demo/views.py
@@ -10,20 +10,16 @@ def funcionStart(request):
     
 
 def resSimp(request):
-    proc = subprocess.Popen(['cat', '-'], stdout=subprocess.PIPE, )
-    #proc = subprocess.Popen(['.matefacil_demo/Programs/'])
-    msg = 'through stdin to stdout'.encode('utf-8')
-    stdout_value = proc.communicate(msg)[0]
-    h = 'pass through: ', stdout_value
-    return HttpResponse(h)
+    output = subprocess.run(["cat", "matefacil_demo/Programs/data.txt"], capture_output=True)
+    return HttpResponse(output)
 
 def resSimpCpp(request):
-    p = subprocess.Popen (["./matefacil_demo/Programs/out1"],stdout=subprocess.PIPE, shell = True)
+    p = subprocess.Popen(["./matefacil_demo/Programs/out1"],stdout=subprocess.PIPE, shell = True)
     m = p.communicate()[0].decode('utf-8')
     return HttpResponse(m)
 
 def resCpp(request):
-    p = subprocess.Popen (["./matefacil_demo/Programs/out2"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell = True)
+    p = subprocess.Popen(["./matefacil_demo/Programs/out2"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell = True)
     m = "%r" %request.GET["cppRes"]
     a = m.replace("'", "")
     p.stdin.write(f'{a}\n'.encode('utf-8'))
@@ -36,11 +32,11 @@ def resJav(request):
     a1 = m1.replace("'", "")
     a2 = m2.replace("'", "")
     msj = f'{a1}{a2}\n'.encode('utf-8')
-    p = subprocess.Popen ([f'java matefacil_demo/Programs/HelloWorld {a1} {a2}'], stdout=subprocess.PIPE, shell = True)
+    p = subprocess.Popen([f'java matefacil_demo/Programs/HelloWorld {a1} {a2}'], stdout=subprocess.PIPE, shell = True)
     s = p.communicate(msj)[0].decode('utf-8')
     return HttpResponse(s)
 
 def resC(request):
-    p = subprocess.Popen (["./matefacil_demo/Programs/out3"], stdout=subprocess.PIPE, shell = True)
+    p = subprocess.Popen(["./matefacil_demo/Programs/out3"], stdout=subprocess.PIPE, shell = True)
     s = p.communicate()[0].decode('utf-8')
     return HttpResponse(s)
